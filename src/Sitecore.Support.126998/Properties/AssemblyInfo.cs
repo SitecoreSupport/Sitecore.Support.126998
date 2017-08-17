187819c4132823cd3ab3f78bb1446b1e9d213333
AriaAkbariyeh <ara@sitecore.net>
2017-08-17 15:59:35 +0800

    Create patch

---
+++ src/Sitecore.Support.126998/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.126998")]
+[assembly: AssemblyProduct("Sitecore.Support.126998")]
+[assembly: ComVisible(false)]
